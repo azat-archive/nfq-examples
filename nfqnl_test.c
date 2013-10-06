119bb83bcbcf459a300729b380f5fd613a18253b
Azat Khuzhin <a3at.mail@gmail.com>
2013-10-06 22:55:08 +0400

    nfqnl_test: drop test printing, and add printing of TCP packet

--- nfqnl_test.c
+++ nfqnl_test.c
@@ -9,67 +9,44 @@
 #include <limits.h>
 
 #include <libnetfilter_queue/libnetfilter_queue.h>
+#define bool int
+#include <libnetfilter_queue/pktbuff.h>
+#undef bool
+#include <libnetfilter_queue/libnetfilter_queue_tcp.h>
+#include <libnetfilter_queue/libnetfilter_queue_ipv4.h>
 
-/* returns packet id */
-static u_int32_t print_pkt (struct nfq_data *tb)
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+	      struct nfq_data *nfa, void *arg)
 {
+	char buf[PATH_MAX] __attribute__ ((aligned));
 	int id = 0;
 	struct nfqnl_msg_packet_hdr *ph;
 	struct nfqnl_msg_packet_hw *hwph;
-	u_int32_t mark,ifi; 
-	int ret;
+	struct tcphdr *tcp;
+	struct iphdr *ip;
 	unsigned char *data;
+	int packet_len;
+	struct pkt_buff *pkt;
 
-	ph = nfq_get_msg_packet_hdr(tb);
+	ph = nfq_get_msg_packet_hdr(nfa);
 	if (ph) {
 		id = ntohl(ph->packet_id);
-		printf("hw_protocol=0x%04x hook=%u id=%u ",
-			ntohs(ph->hw_protocol), ph->hook, id);
+	}
+	hwph = nfq_get_packet_hw(nfa);
+	packet_len = nfq_get_payload(nfa, &data);
+	pkt = pktb_alloc(AF_INET /* TODO */, data, packet_len, 0);
+	ip = nfq_ip_get_hdr(pkt);
+	nfq_ip_set_transport_header(pkt, ip);
+	tcp = nfq_tcp_get_hdr(pkt);
+	if (tcp) {
+		nfq_tcp_snprintf(buf, PATH_MAX, tcp);
+		printf("%s\n", buf);
+	} else {
+		printf("%s\n", "NOT TCP");
 	}
 
-	hwph = nfq_get_packet_hw(tb);
-	if (hwph) {
-		int i, hlen = ntohs(hwph->hw_addrlen);
-
-		printf("hw_src_addr=");
-		for (i = 0; i < hlen-1; i++)
-			printf("%02x:", hwph->hw_addr[i]);
-		printf("%02x ", hwph->hw_addr[hlen-1]);
-	}
-
-	mark = nfq_get_nfmark(tb);
-	if (mark)
-		printf("mark=%u ", mark);
-
-	ifi = nfq_get_indev(tb);
-	if (ifi)
-		printf("indev=%u ", ifi);
-
-	ifi = nfq_get_outdev(tb);
-	if (ifi)
-		printf("outdev=%u ", ifi);
-	ifi = nfq_get_physindev(tb);
-	if (ifi)
-		printf("physindev=%u ", ifi);
-
-	ifi = nfq_get_physoutdev(tb);
-	if (ifi)
-		printf("physoutdev=%u ", ifi);
-
-	ret = nfq_get_payload(tb, &data);
-	if (ret >= 0)
-		printf("payload_len=%d ", ret);
-
-	fputc('\n', stdout);
-
-	return id;
-}
-	
-
-static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
-	      struct nfq_data *nfa, void *data)
-{
-	u_int32_t id = print_pkt(nfa);
+	pktb_free(pkt);
 	return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
 }
 
